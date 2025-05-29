68fda9c1c5b7dceae8c0d42d9ddb75503c637b3a
abida-sultana-ake <151779423+abida-sultana-ake@users.noreply.github.com>
2025-05-29 17:05:39 +0600

    Updated

--- index.js
+++ index.js
@@ -1,20 +1,64 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const app = express();
 const port = process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('NextHire!')
-})
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ux6cwua.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    //job collection
+    const jobsCollection = client.db("NextHire").collection("Jobs");
+
+    //jobs API
+    //find
+    app.get("/jobs", async (req, res) => {
+      const cursor = jobsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    //find single
+    app.get("/jobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobsCollection.findOne(query);
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("NextHire!");
+});
 
 app.listen(port, () => {
-  console.log(`Carrer Code is cooking ${port}`)
-})
+  console.log(`Carrer Code is cooking ${port}`);
+});
 
 app.listen(port, () => {
-    console.log(`Carrer Code server is on port ${port}`)
-})
+  console.log(`Carrer Code server is on port ${port}`);
+});
